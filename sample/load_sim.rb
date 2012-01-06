2ed4c4fe0974857f39d2fa168218b0149fc42705
Sean McDaniel <seanmcdaniel@me.com>
2012-01-05 22:46:57 -0500

    better load sim

---
+++ sample/load_sim.rb
@@ -0,0 +1,19 @@
+$: << ::File.expand_path("../../lib/", __FILE__)
+require "rubygems"
+require "bundler/setup"
+require 'redmon'
+require 'redis'
+
+redis = Redis.connect
+
+loop do
+  start = rand(100000)
+  multi = rand(5)
+  start.upto(multi * start) do |i|
+    redis.set("key-#{i}", "abcdedghijklmnopqrstuvwxyz")
+  end
+
+  start.upto(multi   * start) do |i|
+    redis.del("key-#{i}")
+  end
+end
