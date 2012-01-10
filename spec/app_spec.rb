d85067cb0a2d8352e9d893b8589216d186f83886
Sean McDaniel <sean.mcdaniel@me.com>
2012-01-10 16:00:51 -0500

    tabs to spaces

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,24 +10,24 @@ describe "app" do
 
   context "/" do
     it "should render the app" do
-    	get "/"
-    	last_response.should be_ok
-    	last_response.body.include?('Redmon')
+      get "/"
+      last_response.should be_ok
+      last_response.body.include?('Redmon')
     end
   end
 
   context "/info" do
     it "should render json" do
-    	get "/info"
-    	last_response.should be_ok
-    	last_response.headers["Content-Type"].should == "application/json;charset=utf-8"
+      get "/info"
+      last_response.should be_ok
+      last_response.headers["Content-Type"].should == "application/json;charset=utf-8"
     end
 
     it "should render the correct # of results" do
-    	get "/info?count=1"
+      get "/info?count=1"
       JSON.parse(last_response.body).length.should == 1
 
-    	get "/info?count=2"
+      get "/info?count=2"
       JSON.parse(last_response.body).length.should == 2
     end
   end
