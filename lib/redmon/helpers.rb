ddd781efe7ea055abf95ffe941d5b3de910a7c26
Howl王 <mimosa@shareday.com>
2012-07-11 15:35:55 +0800

    add URLMap support.

--- lib/redmon/helpers.rb
+++ lib/redmon/helpers.rb
@@ -1,6 +1,16 @@
 module Redmon
   module Helpers
     include Redmon::Redis
+    include Rack::Utils
+
+    def url_path(*path_parts)
+      [ path_prefix, path_parts ].join("/").squeeze('/')
+    end
+    alias_method :u, :url_path
+
+    def path_prefix
+      request.env['SCRIPT_NAME']
+    end
 
     def prompt
       "#{redis_url.gsub('://', ' ')}>"
